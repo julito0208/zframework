1d551d94c9be553351935b7a5b61c34e5429237f
julito0208 <skywalker86>
2016-07-25 22:08:09 -0300

    updates

--- static/js/plugins/listmenu/listmenu.js
+++ static/js/plugins/listmenu/listmenu.js
@@ -1,8 +1,8 @@
 
-
-jQuery.fn.listMenu = function() {
-
-    var $this = $(this);
+jQuery.listMenu = function(node, options)
+{
+    var $this = $(node);
+    options = $.extend({}, jQuery.listMenu.defaultOptions, options);
 
     $this.triggerHandler('listMenu.init');
 
@@ -50,17 +50,32 @@ jQuery.fn.listMenu = function() {
         var link = $(this);
         var ul = link.next();
 
-        if(ul.is(':visible'))
+        if(options['resumeUnique'] && ul.children().length == 1)
         {
-            ul.removeClass('visible').slideUp();
-            link.find('.icon').removeClass('fa-minus-square').addClass('fa-plus-square');
-            $this.triggerHandler('listMenu.hideItem', ul);
+            var link = ul.find('li a');
+
+            if(link.attr('href'))
+            {
+                location.href = link.attr('href');
+            }
+
+            link.trigger('click');
+
         }
         else
         {
-            ul.addClass('visible').slideDown();
-            link.find('.icon').removeClass('fa-plus-square').addClass('fa-minus-square');
-            $this.triggerHandler('listMenu.showItem', ul);
+            if(ul.is(':visible'))
+            {
+                ul.removeClass('visible').slideUp();
+                link.find('.icon').removeClass('fa-minus-square').addClass('fa-plus-square');
+                $this.triggerHandler('listMenu.hideItem', ul);
+            }
+            else
+            {
+                ul.addClass('visible').slideDown();
+                link.find('.icon').removeClass('fa-plus-square').addClass('fa-minus-square');
+                $this.triggerHandler('listMenu.showItem', ul);
+            }
         }
 
         $this.triggerHandler('listMenu.clickItem');
@@ -83,8 +98,57 @@ jQuery.fn.listMenu = function() {
         $this.find('.menu-submenu-link > a').prepend("<span class='icon fa fa-plus-square'></span>&nbsp;&nbsp;");
 
         $this.find('.menu-submenu-link li.active').getParent('li.menu-item').find('a .icon').removeClass('fa-plus-square').addClass('fa-minus-square');
+
     }
 
-    $this.find('.menu-sublink.active').parents().not($this).show();
+    if(options['resumeUnique'])
+    {
+        $this.find('.menu-submenu-link > a').each(function(index, item) {
 
+            var submenu = $(this).next();
+
+            if(submenu.children().length == 1)
+            {
+                submenu.getParent().addClass('unique');
+                submenu.getParent().children('a').find('.icon').hide();
+            }
+
+        });
+    }
+
+    $this.find('.menu-sublink.active').getParent().getParent().addClass('active');
+
+    if(options['resumeUnique'])
+    {
+        var submenuItemActive = $this.find('.menu-sublink.active');
+
+        if(submenuItemActive.length > 0)
+        {
+            var submenuListActive = submenuItemActive.getParent();
+
+            if(submenuListActive.children().length > 1)
+            {
+                $this.find('.menu-sublink.active').parents().not($this).show();
+                submenuListActive.getParent().addClass('active');
+            }
+        }
+    }
+    else
+    {
+        $this.find('.menu-sublink.active').parents().not($this).show();
+        $this.find('.menu-sublink.active').getParent().getParent().addClass('active');
+    }
+
+
+
+};
+
+jQuery.listMenu.defaultOptions = {
+    'resumeUnique': true
+};
+
+
+jQuery.fn.listMenu = function(options) {
+    $.listMenu(this, options);
+    return this;
 };
