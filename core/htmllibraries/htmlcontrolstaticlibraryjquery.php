cccba8721404bdf6cbf193a47389b5a4f6c91cec
julito0208 <skywalker86>
2016-12-12 13:14:07 -0300

    Updates

--- core/htmllibraries/htmlcontrolstaticlibraryjquery.php
+++ core/htmllibraries/htmlcontrolstaticlibraryjquery.php
@@ -6,27 +6,12 @@ class HTMLControlStaticLibraryJQuery extends HTMLControlStaticLibrary {
 
 		$jquery_files = array(URLHelper::get_zframework_static_url('thirdparty/jquery/jquery.min.js'));
 
-		if(ZPHP::is_mobile())
-		{
-			$jquery_ui = self::_create_instance(self::STATIC_LIBRARY_JQUERYUI);
-			$jquery_files = array_merge($jquery_files, $jquery_ui->get_library_js_files());
-
-			$jquery_mobile = self::_create_instance(self::STATIC_LIBRARY_JQUERY_MOBILE);
-			$jquery_files = array_merge($jquery_files, $jquery_mobile->get_library_js_files());
-		}
-
 		return $jquery_files;
 	}
 	
 	public function get_library_css_files() {
 
 		$jquery_files = array();
-
-		if(ZPHP::is_mobile())
-		{
-			$jquery_mobile = self::_create_instance(self::STATIC_LIBRARY_JQUERY_MOBILE);
-			$jquery_files = array_merge($jquery_files, $jquery_mobile->get_library_css_files());
-		}
 
 		return $jquery_files;
 
